d3300c98ea6ee322b0730ea8e1218a2c259a0405
Midorichie <hamsohood@gmail.com>
2023-10-07 11:48:16 +0100

    Ascending

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,26 +5,19 @@
  */
 int main(void)
 {
-int d1, d2, d3;
+int n;
 
-for (d1 = 0; d1 <= 9; d1++)
+for (n = 0; n <= 9; n++)
 {
-	for (d2 = d1 + 1; d2 <= 9; d2++)
-	{
-		for (d3 = d2 + 1; d3 <= 9; d3++)
-		{
-			putchar(d1 + '0');
-			putchar(d2 + '0');
-			putchar(d3 + '0');
-			if (d1 != 7 || d2 != 8 || d3 != 9)
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
-	}
+putchar(n + '0');
+if (n != 9)
+{
+putchar(',');
+putchar(' ');
 }
+}
+
 putchar('\n');
+
 return (0);
 }
-
