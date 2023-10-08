e706f282b696ef112466c61ef40743137e37772e
Midorichie <hamsohood@gmail.com>
2023-10-08 10:18:49 +0100

    print comb

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,22 +9,28 @@ int main(void)
 
 	for (i = 0; i <= 99; i++)
 	{
-		for (j = i; j <= 99; j++)
-		{
-			putchar((i / 10) + '0');
-			putchar((i % 10) + '0');
-			putchar(' ');
-			putchar((j / 10) + '0');
-			putchar((j % 10) + '0');
+	for (j = i; j <= 99; j++)
+	{
+		int first_digit_i = i / 10;
+		int second_digit_i = i % 10;
+		int first_digit_j = j / 10;
+		int second_digit_j = j % 10;
 
-			if (i != 99 || j != 99)
-			{
-				putchar(',');
-				putchar(' ');
-			}
+		putchar(first_digit_i + '0');
+		putchar(second_digit_i + '0');
+		putchar(' ');
+		putchar(first_digit_j + '0');
+		putchar(second_digit_j + '0');
+
+		if (i != 99 || j != 99)
+		{
+			putchar(',');
+			putchar(' ');
 		}
 	}
+}
 
-	putchar('\n');
-	return (0);
+putchar('\n');
+
+return (0);
 }
