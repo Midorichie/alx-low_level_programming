5af02f04526e478c557db7581f4db49ea6804134
Midorichie <hamsohood@gmail.com>
2023-10-09 12:42:59 +0100

    Territory

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,14 @@
 #include <unistd.h>
+#include <stdio.h>
 /**
  * main - A c program that prints a line to the standard error
  * Return: 1 (Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korper, 2015-10-19\n", 59);
-	return (1);
+	write(1, "and that piece of art is useful\" - Dora Korper, 2015-10-19\n", 59);
+
+	write(2 "\n", 1);
+
+	return (0);
 }
