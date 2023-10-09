f67b67211c0bbc933461ed6dd198fb1dd5bd0e0b
Midorichie <hamsohood@gmail.com>
2023-10-09 15:06:14 +0100

    Software

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,18 +12,13 @@ int main(void)
 	{
 		for (j = 1; j < 99; j++)
 		{
-			int first_digit_i = i / 10;
-			int second_digit_i = i % 10;
-			int first_digit_j = j / 10;
-			int second_digit_j = j % 10;
-
-			putchar(first_digit_i + '0');
-			putchar(second_digit_i + '0');
+			putchar('0' + (i / 10));
+			putchar('0' + (i % 10));
 			putchar(' ');
-			putchar(first_digit_j + '0');
-			putchar(second_digit_j + '0');
+			putchar('0' + (j / 10));
+			putchar('0' + (j % 10));
 
-			if (i != 99 || j != 99)
+			if (i != 98 || j != 99)
 			{
 				putchar(',');
 				putchar(' ');
