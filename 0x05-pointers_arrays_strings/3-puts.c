3b479ae169d85b5af32a34ef2634ac4be5425ea2
Midorichie <hamsohood@gmail.com>
2023-10-17 02:58:13 +0100

    Puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - function that prints a string
+ * @str: pointer to the string
+ * Return: string and new line
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		putchar(str[i]);
+		i++;
+	}
+	putchar('\n');
+}
