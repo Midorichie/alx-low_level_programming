eecb3484e39bda298d6230f358b157a76a6726c2
Midorichie <hamsohood@gmail.com>
2023-10-18 02:01:47 +0100

    Convert string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -5,34 +5,40 @@
  * Desc: This code demonstrates the implementation of a function
  * that converts a string to an integer
  * @s: The string that is converted to an integer
- * Return: sign * result
+ * Return: the int converted to a string
  */
 
 int _atoi(char *s)
 {
-	int sign = 1;
-	int result = 0;
-	int i = 0;
+	int i, d, n, len, f, digit;
 
-	while (s[i] == ' ')
-	{
-		i++;
-	}
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
 
-	if (s[i] == '-' || s[i] == '+')
+	while (s[len] != '\0')
+		len++;
+	while (i < len && f == 0)
 	{
 		if (s[i] == '-')
+			++d;
+		if (s[i] >= '0' && s[i] <= '9')
 		{
-			sign = -1;
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
 		}
 		i++;
 	}
-
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-		result = result * 10 + (s[i] - '0');
-		i++;
-	}
-
-	return (sign * result);
+	if (f == 0)
+		return (0);
+	return (n);
 }
