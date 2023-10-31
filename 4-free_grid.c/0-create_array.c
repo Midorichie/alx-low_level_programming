74efc67b570d8241879684725e752372d41bf616
Midorichie <hamsohood@gmail.com>
2023-10-31 11:44:03 +0100

    Float like a butterfly, sting like a bee

---
+++ 4-free_grid.c/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of chars and
+ * initializes it with a specific char
+ * @size: size of the array
+ * @c: char value to initialize the array with
+ *
+ * Return: A pointer to the array, or NULL if it fails
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int i;
+
+	if (size == 0)
+		return (0);
+
+	array = malloc(size * sizeof(char));
+	if (array == (0))
+		return (0);
+	for (i = 0; i < size; i++)
+		array[i] = c;
+
+	return (array);
+}
