2eb9cdbe13444fbd7aede7e19c57f44e8da0b811
Midorichie <hamsohood@gmail.com>
2023-11-06 19:25:31 +0100

    File name

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("Compiled from file: %s\n", __FILE__);
+	return (0);
+}
